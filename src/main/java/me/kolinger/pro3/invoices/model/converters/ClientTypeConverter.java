6b809a8216f4f3e65f4189d22aec62b67f7f9661
Tomáš Kolinger <tomas@kolinger.name>
2014-02-28 20:22:05 +0100

    implemented clients CRUD

---
+++ src/main/java/me/kolinger/pro3/invoices/model/converters/ClientTypeConverter.java
@@ -0,0 +1,33 @@
+package me.kolinger.pro3.invoices.model.converters;
+
+import me.kolinger.pro3.invoices.common.LoggedObject;
+import me.kolinger.pro3.invoices.model.impl.entities.Client;
+import me.kolinger.pro3.invoices.model.impl.services.ClientsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+
+/**
+ * @author Tomáš Kolinger <tomas@kolinger.name>
+ */
+@Component
+public class ClientTypeConverter extends LoggedObject implements Converter {
+
+    @Autowired
+    private ClientsService service;
+
+    public Object getAsObject(FacesContext context, UIComponent component, String value) {
+        if (value == null || value.length() == 0 || value.equals("null")) {
+            return null;
+        }
+        Long id = Long.parseLong(value);
+        return service.findOneById(id);
+    }
+
+    public String getAsString(FacesContext context, UIComponent component, Object value) {
+        return value instanceof Client ? ((Client) value).getId().toString() : null;
+    }
+}
--- src/main/java/me/kolinger/pro3/invoices/model/impl/dao/CompaniesDao.java
+++ src/main/java/me/kolinger/pro3/invoices/model/impl/dao/CompaniesDao.java
@@ -2,6 +2,8 @@ package me.kolinger.pro3.invoices.model.impl.dao;
 
 import me.kolinger.pro3.invoices.model.DeletableDao;
 import me.kolinger.pro3.invoices.model.impl.entities.Company;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Order;
 import org.springframework.stereotype.Repository;
 
 
@@ -13,5 +15,12 @@ public class CompaniesDao extends DeletableDao<Company> {
 
     public CompaniesDao() {
         super(Company.class);
+    }
+
+    @Override
+    protected Criteria createCriteria() {
+        Criteria criteria = super.createCriteria();
+        criteria.addOrder(Order.asc("name"));
+        return criteria;
     }
 }
