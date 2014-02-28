5695524e869760279eb766323085338c7ebcce9d
Tomáš Kolinger <tomas@kolinger.name>
2014-02-28 21:19:20 +0100

    implemented base invoices CRUD

--- src/main/java/me/kolinger/pro3/invoices/beans/manager/InvoicesBean.java
+++ src/main/java/me/kolinger/pro3/invoices/beans/manager/InvoicesBean.java
@@ -1,11 +1,18 @@
 package me.kolinger.pro3.invoices.beans.manager;
 
 import me.kolinger.pro3.invoices.beans.CrudBean;
+import me.kolinger.pro3.invoices.common.Translator;
+import me.kolinger.pro3.invoices.model.impl.entities.Client;
+import me.kolinger.pro3.invoices.model.impl.entities.Company;
 import me.kolinger.pro3.invoices.model.impl.entities.Invoice;
+import me.kolinger.pro3.invoices.model.impl.services.ClientsService;
+import me.kolinger.pro3.invoices.model.impl.services.CompaniesService;
 import me.kolinger.pro3.invoices.model.impl.services.InvoicesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 /**
  * @author Tomáš Kolinger <tomas@kolinger.name>
@@ -13,6 +20,13 @@ import org.springframework.stereotype.Component;
 @Scope("session")
 @Component
 public class InvoicesBean extends CrudBean<Invoice> {
+
+    @Autowired
+    public CompaniesService companiesService;
+
+
+    @Autowired
+    public ClientsService clientsService;
 
     private InvoicesService service;
 
@@ -22,4 +36,21 @@ public class InvoicesBean extends CrudBean<Invoice> {
         this.service = service;
     }
 
+    public List<Company> getCompanies() {
+        return companiesService.findAll();
+    }
+
+    public List<Client> getClients() {
+        return clientsService.findAll();
+    }
+
+    public String[][] getTypes() {
+        return new String[][]{
+                new String[]{Invoice.Type.INVOICE.toString(),
+                        Translator.translate("protected.invoices.type.invoice")},
+                
+                new String[]{Invoice.Type.CREDIT_NOTE.toString(),
+                        Translator.translate("protected.invoices.type.credit_note")},
+        };
+    }
 }
--- src/main/java/me/kolinger/pro3/invoices/model/impl/dao/ClientsDao.java
+++ src/main/java/me/kolinger/pro3/invoices/model/impl/dao/ClientsDao.java
@@ -2,6 +2,8 @@ package me.kolinger.pro3.invoices.model.impl.dao;
 
 import me.kolinger.pro3.invoices.model.DeletableDao;
 import me.kolinger.pro3.invoices.model.impl.entities.Client;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Order;
 import org.springframework.stereotype.Repository;
 
 
@@ -13,5 +15,12 @@ public class ClientsDao extends DeletableDao<Client> {
 
     public ClientsDao() {
         super(Client.class);
+    }
+
+    @Override
+    protected Criteria createCriteria() {
+        Criteria criteria = super.createCriteria();
+        criteria.addOrder(Order.asc("name"));
+        return criteria;
     }
 }
