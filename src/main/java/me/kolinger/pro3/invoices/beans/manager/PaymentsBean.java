3a435f6d246e48c5143d5341a5a13594b48d05a5
Tomáš Kolinger <tomas@kolinger.name>
2014-02-28 22:05:00 +0100

    implemented payments CRUD

--- src/main/java/me/kolinger/pro3/invoices/beans/manager/PaymentsBean.java
+++ src/main/java/me/kolinger/pro3/invoices/beans/manager/PaymentsBean.java
@@ -1,7 +1,10 @@
 package me.kolinger.pro3.invoices.beans.manager;
 
 import me.kolinger.pro3.invoices.beans.CrudBean;
+import me.kolinger.pro3.invoices.common.Translator;
+import me.kolinger.pro3.invoices.model.impl.entities.Invoice;
 import me.kolinger.pro3.invoices.model.impl.entities.Payment;
+import me.kolinger.pro3.invoices.model.impl.services.InvoicesService;
 import me.kolinger.pro3.invoices.model.impl.services.PaymentsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
@@ -14,7 +17,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class PaymentsBean extends CrudBean<Payment> {
 
+    @Autowired
+    public InvoicesService invoicesService;
+
     private PaymentsService service;
+    private Long id;
 
     @Autowired
     public PaymentsBean(PaymentsService service) {
@@ -22,4 +29,24 @@ public class PaymentsBean extends CrudBean<Payment> {
         this.service = service;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void saveEntity() {
+        if (getEntity().getInvoice() == null) {
+            Invoice invoice = invoicesService.findOneById(id);
+            if (invoice == null) {
+                sendErrorGrowl(Translator.translate("protected.payments.id.error_not_found"));
+                return;
+            }
+            getEntity().setInvoice(invoice);
+        }
+
+        super.saveEntity();
+    }
 }
