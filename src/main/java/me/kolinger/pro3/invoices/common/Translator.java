9e39577e409dfec6eaf3ed6b8d356c088c664df9
Tomáš Kolinger <tomas@kolinger.name>
2014-03-17 17:18:37 +0100

    improved static translator

--- src/main/java/me/kolinger/pro3/invoices/common/Translator.java
+++ src/main/java/me/kolinger/pro3/invoices/common/Translator.java
@@ -2,6 +2,7 @@ package me.kolinger.pro3.invoices.common;
 
 import javax.faces.component.UIViewRoot;
 import javax.faces.context.FacesContext;
+import java.util.Locale;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
@@ -19,20 +20,30 @@ public class Translator extends LoggedObject {
         return getInstance().translateKey(key);
     }
 
-    private Translator() {
-        FacesContext facesContext = FacesContext.getCurrentInstance();
-        UIViewRoot viewRoot = facesContext.getViewRoot();
-        bundle = ResourceBundle.getBundle(BUNDLE_NAME, viewRoot.getLocale());
+    private Translator(Locale locale) {
+        if (locale == null) {
+            FacesContext facesContext = FacesContext.getCurrentInstance();
+            UIViewRoot viewRoot = facesContext.getViewRoot();
+            locale = viewRoot.getLocale();
+        }
+        bundle = ResourceBundle.getBundle(BUNDLE_NAME, locale);
     }
 
-    private static Translator getInstance() {
+    public static Translator getInstance() {
         if (instance == null) {
-            instance = new Translator();
+            instance = new Translator(null);
         }
         return instance;
     }
 
-    private String translateKey(String key) {
+    public static Translator getInstance(Locale locale) {
+        if (instance == null) {
+            instance = new Translator(locale);
+        }
+        return instance;
+    }
+
+    public String translateKey(String key) {
         try {
             Object value = bundle.getObject(key);
             return (String) value;
